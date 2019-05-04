050c7a3f52863ca764864e72a1b14897189bf0ac
Gabriel Almeida <gabrielf.almeida90@gmail.com>
2019-05-03 21:44:57 -0300

    Criadas entidades das tabelas

---
+++ CsStats/src/csstats/TbEquipesEntity.java
@@ -0,0 +1,105 @@
+package csstats;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_equipes", schema = "public", catalog = "APS")
+public class TbEquipesEntity {
+    private int idEquipe;
+    private String nome;
+    private byte[] imagem;
+    private String origem;
+    private Boolean ativo;
+    private Collection<TbCampeonatoEquipesStatusEntity> tbCampeonatoEquipesStatusesByIdEquipe;
+    private Collection<TbJogadorEquipeEntity> tbJogadorEquipesByIdEquipe;
+
+    @Id
+    @Column(name = "id_equipe", nullable = false)
+    public int getIdEquipe() {
+        return idEquipe;
+    }
+
+    public void setIdEquipe(int idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    @Basic
+    @Column(name = "nome", nullable = true, length = 60)
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Basic
+    @Column(name = "imagem", nullable = true)
+    public byte[] getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(byte[] imagem) {
+        this.imagem = imagem;
+    }
+
+    @Basic
+    @Column(name = "origem", nullable = true, length = 60)
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    @Basic
+    @Column(name = "ativo", nullable = true)
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TbEquipesEntity that = (TbEquipesEntity) o;
+        return idEquipe == that.idEquipe &&
+                Objects.equals(nome, that.nome) &&
+                Arrays.equals(imagem, that.imagem) &&
+                Objects.equals(origem, that.origem) &&
+                Objects.equals(ativo, that.ativo);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(idEquipe, nome, origem, ativo);
+        result = 31 * result + Arrays.hashCode(imagem);
+        return result;
+    }
+
+    @OneToMany(mappedBy = "tbEquipesByIdEquipe")
+    public Collection<TbCampeonatoEquipesStatusEntity> getTbCampeonatoEquipesStatusesByIdEquipe() {
+        return tbCampeonatoEquipesStatusesByIdEquipe;
+    }
+
+    public void setTbCampeonatoEquipesStatusesByIdEquipe(Collection<TbCampeonatoEquipesStatusEntity> tbCampeonatoEquipesStatusesByIdEquipe) {
+        this.tbCampeonatoEquipesStatusesByIdEquipe = tbCampeonatoEquipesStatusesByIdEquipe;
+    }
+
+    @OneToMany(mappedBy = "tbEquipesByIdEquipe")
+    public Collection<TbJogadorEquipeEntity> getTbJogadorEquipesByIdEquipe() {
+        return tbJogadorEquipesByIdEquipe;
+    }
+
+    public void setTbJogadorEquipesByIdEquipe(Collection<TbJogadorEquipeEntity> tbJogadorEquipesByIdEquipe) {
+        this.tbJogadorEquipesByIdEquipe = tbJogadorEquipesByIdEquipe;
+    }
+}
